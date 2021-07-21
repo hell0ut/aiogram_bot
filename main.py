16ab23d7b6e152b8e9be803939a0c2e059637e30
hell0ut <43760986+hell0ut@users.noreply.github.com>
2021-07-21 14:36:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -633,7 +633,8 @@ async def on_shutdown(dp):
     # insert code here to run it before shutdown
     pass
 
-asyncio.run(main())
+
 start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
               on_startup=on_startup, on_shutdown=on_shutdown,
               host=WEBAPP_HOST, port=WEBAPP_PORT)
+asyncio.run(main())
