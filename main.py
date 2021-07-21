b43e8b955330fe3dba3152ca94b891e51682c8cc
hell0ut <43760986+hell0ut@users.noreply.github.com>
2021-07-21 14:33:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 
 from aiogram.dispatcher.filters.state import State, StatesGroup
-
+from aiogram.utils.executor import start_webhook
 
 class States(StatesGroup):
     START_STATE = State()
@@ -624,5 +624,16 @@ async def main():
         await dp.storage.wait_closed()
         await bot.session.close()
 
+        
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    
+    
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
 
 asyncio.run(main())
+start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+              on_startup=on_startup, on_shutdown=on_shutdown,
+              host=WEBAPP_HOST, port=WEBAPP_PORT)
